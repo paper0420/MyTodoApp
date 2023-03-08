5f260697ab0dac0816694cf5e27a1d770fcafa64
Paper0420 <sumitra0420@gmail.com>
2023-03-08 18:51:12 +1100

    feat(client): create draft ui

--- client/src/app/components/todo-item/todo-item.component.ts
+++ client/src/app/components/todo-item/todo-item.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent {
-
+  isTranslateClicked: boolean = false;
+  translateClick() {
+    this.isTranslateClicked = !this.isTranslateClicked;
+  }
 }
